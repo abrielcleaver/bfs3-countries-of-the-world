be1a051a2554c199b98ff9fb9f7783a425885a1e
Abriel Cleaver <abrielcleaver@gmail.com>
2021-12-08 18:30:41 -0700

    list of countries display on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react';
+import { getCountries } from './services/countries';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getCountries();
+      setCountries(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  if (loading) return <h1>Please wait....</h1>;
   return (
     <div className="App">
       <h1>Countries of the World</h1>
+      {countries.map((c) => (
+        <p key={c.id}>{c.name}</p>
+      ))}
     </div>
   );
 }
