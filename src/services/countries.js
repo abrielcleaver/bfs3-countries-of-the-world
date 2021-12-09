02e8af7267b1a6e50be9c52cf8e90b96a585e5e2
Abriel Cleaver <abrielcleaver@gmail.com>
2021-12-08 18:51:01 -0700

    removing console log

--- src/services/countries.js
+++ src/services/countries.js
@@ -3,6 +3,6 @@ import { checkError, client } from './client';
 export async function getCountries() {
   // call supabase and get list of countries
   const response = await client.from('countries').select();
-  console.log(response);
+  
   return checkError(response);
 }
